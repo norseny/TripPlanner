61952c40f1e97b22e237e1f2476d8dd26ef78538
bea <beata.piechowicz@student.uj.edu.pl>
2018-05-19 15:36:22 +0200

    Pdf utils more info added

--- tripplanner/pdf_utils.py
+++ tripplanner/pdf_utils.py
@@ -2,7 +2,7 @@ from reportlab.lib import colors
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 from reportlab.lib.pagesizes import letter, A4, landscape
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table,\
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, \
     TableStyle
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
@@ -11,7 +11,6 @@ from django.utils.translation import gettext as _
 
 
 class PdfPrint:
-
     # initialize class
     def __init__(self, buffer, pageSize):
         self.buffer = buffer
@@ -22,11 +21,13 @@ class PdfPrint:
         elif pageSize == 'Letter':
             self.pageSize = letter
         self.width, self.height = self.pageSize
-        a =9
+        a = 9
 
-        pdfmetrics.registerFont(TTFont('RobotoCondensed-Regular', 'static/libs/font-roboto/RobotoCondensed-Regular.ttf'))
+        pdfmetrics.registerFont(
+            TTFont('RobotoCondensed-Regular', 'static/libs/font-roboto/RobotoCondensed-Regular.ttf'))
         pdfmetrics.registerFont(TTFont('RobotoBold', 'static/libs/font-roboto/Roboto-Bold.ttf'))
-        pdfmetrics.registerFont(TTFont('RobotoCondensed-BoldItalic', 'static/libs/font-roboto/RobotoCondensed-BoldItalic.ttf'))
+        pdfmetrics.registerFont(
+            TTFont('RobotoCondensed-BoldItalic', 'static/libs/font-roboto/RobotoCondensed-BoldItalic.ttf'))
 
     def report(self, context, title):
         # set some characteristics for pdf document
@@ -37,50 +38,52 @@ class PdfPrint:
             topMargin=20,
             bottomMargin=20,
             pagesize=self.pageSize,
-            )
+        )
 
         # a collection of styles offer by the library
         styles = getSampleStyleSheet()
 
         # add custom paragraph style
         styles.add(ParagraphStyle(
-            name="MainInfo",fontName='RobotoBold',
+            name="MainInfo", fontName='RobotoBold',
             fontSize=12
         ))
         styles.add(ParagraphStyle(
-            name="TableHeader",fontName='RobotoBold',
+            name="TableHeader", fontName='RobotoBold',
             fontSize=11, alignment=TA_CENTER,
         ))
         styles.add(ParagraphStyle(
-            name="MyNormal",fontName='RobotoCondensed-Regular',
-            fontSize=10,leading=12
+            name="MyNormal", fontName='RobotoCondensed-Regular',
+            fontSize=10, leading=12
         ))
-        styles.add(ParagraphStyle( #todo: align to right
-            name="MyNormalDecimal",fontName='RobotoCondensed-Regular',
+        styles.add(ParagraphStyle(  # todo: align to right
+            name="MyNormalDecimal", fontName='RobotoCondensed-Regular',
             fontSize=10, aligment=TA_RIGHT,
         ))
         styles.add(ParagraphStyle(
-            name="Categories",fontName='RobotoCondensed-BoldItalic',
-            fontSize=11,leading=12
+            name="Categories", fontName='RobotoCondensed-BoldItalic',
+            fontSize=11, leading=12
         ))
 
         # list used for elements added into document
         data = []
-        data.append(Spacer(1, 12))
+        # data.append(Spacer(1, 4))
         data.append(Paragraph(title, styles['Title']))
         # insert a blank space
-        data.append(Spacer(1, 24))
+        data.append(Spacer(1, 12))
 
-        (data.append(Paragraph(_('Description') + ': ' + context['trip'].description, styles['MainInfo']))) if context['trip'].description else ''
-        data.append(Spacer(1, 12))
+        (data.append(Paragraph(_('Description') + ': ' + context['trip'].description, styles['MainInfo']))) if context[
+            'trip'].description else ''
+        data.append(Spacer(1, 4))
         data.append(Paragraph(_('Created by') + ': ' + str(context['trip'].created_by), styles['MainInfo']))
-        data.append(Spacer(1, 12))
+        data.append(Spacer(1, 4))
         data.append(Paragraph(_('Total Cost') + ': ' + str(context['trip'].price), styles['MainInfo']))
-        data.append(Spacer(1, 24))
+        data.append(Spacer(1, 12))
 
         table_data = []
         # table header
         table_data.append([
+            Paragraph(_('Category/ No.'), styles['TableHeader']),
             Paragraph(_('Name'), styles['TableHeader']),
             Paragraph(_('Start time'), styles['TableHeader']),
             Paragraph(_('End time'), styles['TableHeader']),
@@ -90,8 +93,8 @@ class PdfPrint:
             Paragraph(_('Address'), styles['TableHeader']),
             Paragraph(_('Cost'), styles['TableHeader'])
         ])
-        table_data.append([Paragraph(_('Journeys'), styles['Categories']),'','','','','','',''])
-        for journey in context['trip'].journey_set.all():
+        table_data.append([Paragraph(_('Journeys'), styles['Categories']), '', '', '', '', '', '', ''])
+        for i, journey in enumerate(context['trip'].journey_set.all()):
             if isinstance(journey.start_time, datetime):
                 start_time = (journey.start_time).strftime('%d.%m.%Y (%H:%M)')
             else:
@@ -101,18 +104,21 @@ class PdfPrint:
             else:
                 end_time = ''
             table_data.append(
-                [Paragraph(formatTableCellData(journey.name), styles['MyNormal']),
-                 Paragraph(formatTableCellData(start_time), styles['MyNormal']),
-                 Paragraph(formatTableCellData(end_time), styles['MyNormal']),
-                 Paragraph(formatTableCellData(journey.start_point), styles['MyNormal']),
-                 Paragraph(formatTableCellData(journey.end_point), styles['MyNormal']),
-                 Paragraph(formatTableCellData(journey.means_of_transport), styles['MyNormal']),
-                 '',
-                 Paragraph(formatTableCellData(journey.price), styles['MyNormalDecimal']),
-                 ]
+                [
+                    Paragraph(str(i + 1), styles['MyNormal']),
+                    # Paragraph(formatTableCellData(journey.name), styles['MyNormal']),
+                    '',
+                    Paragraph(formatTableCellData(start_time), styles['MyNormal']),
+                    Paragraph(formatTableCellData(end_time), styles['MyNormal']),
+                    Paragraph(formatTableCellData(journey.start_point), styles['MyNormal']),
+                    Paragraph(formatTableCellData(journey.end_point), styles['MyNormal']),
+                    Paragraph(formatTableCellData(journey.means_of_transport), styles['MyNormal']),
+                    '',
+                    Paragraph(formatTableCellData(journey.price), styles['MyNormalDecimal']),
+                ]
             )
-        table_data.append([Paragraph(_('Accommodations'), styles['Categories']),'','','','','','',''])
-        for accommodation in context['trip'].accommodation_set.all():
+        table_data.append([Paragraph(_('Accommodations'), styles['Categories']), '', '', '', '', '', '', ''])
+        for i, accommodation in enumerate(context['trip'].accommodation_set.all()):
             if isinstance(accommodation.start_time, datetime):
                 start_time = (accommodation.start_time).strftime('%d.%m.%Y (%H:%M)')
             else:
@@ -124,28 +130,31 @@ class PdfPrint:
 
             table_data.append(
                 [
-                Paragraph(formatTableCellData(accommodation.name), styles['MyNormal']),
-                Paragraph(formatTableCellData(start_time),
-                          styles['MyNormal']),
-                Paragraph(formatTableCellData(end_time),
-                                              styles['MyNormal']),
-                '',
-                '',
-                '',
-                Paragraph(formatTableCellData(accommodation.address), styles['MyNormal']),
-                Paragraph(formatTableCellData(accommodation.price), styles['MyNormalDecimal']),
+                    Paragraph(str(i + 1), styles['MyNormal']),
+                    Paragraph(formatTableCellData(accommodation.name), styles['MyNormal']),
+                    Paragraph(formatTableCellData(start_time),
+                              styles['MyNormal']),
+                    Paragraph(formatTableCellData(end_time),
+                              styles['MyNormal']),
+                    '',
+                    '',
+                    '',
+                    Paragraph(formatTableCellData(accommodation.address), styles['MyNormal']),
+                    Paragraph(formatTableCellData(accommodation.price), styles['MyNormalDecimal']),
                 ]
             )
-        table_data.append([Paragraph(_('Attractions'), styles['Categories']),'','','','','','',''])
-        for attraction in context['trip'].attraction_set.all():
+        table_data.append([Paragraph(_('Attractions'), styles['Categories']), '', '', '', '', '', '', ''])
+        for i, attraction in enumerate(context['trip'].attraction_set.all()):
             if isinstance(attraction.start_time, datetime):
                 start_time = (attraction.start_time).strftime('%d.%m.%Y (%H:%M)')
             else:
                 start_time = ''
             table_data.append(
-                [Paragraph(formatTableCellData(attraction.name), styles['MyNormal']),
+                [
+                Paragraph(str(i + 1), styles['MyNormal']),
+                Paragraph(formatTableCellData(attraction.name), styles['MyNormal']),
                  Paragraph(formatTableCellData(start_time),
-                                               styles['MyNormal']),
+                           styles['MyNormal']),
                  '',
                  '',
                  '',
@@ -155,7 +164,7 @@ class PdfPrint:
                  ]
             )
         # create table
-        trip_details_table = Table(table_data, colWidths=[doc.width/8.0]*8)
+        trip_details_table = Table(table_data, colWidths=[doc.width / 9.0] * 9)
         trip_details_table.setStyle(TableStyle(
             [
                 ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
@@ -165,7 +174,25 @@ class PdfPrint:
             ]
         ))
         data.append(trip_details_table)
-        data.append(Spacer(1, 48))
+        data.append(Spacer(1, 12))
+
+        # data.append(Paragraph((_('More info')), styles['MainInfo']))
+        data.append(Spacer(1, 4))
+        data.append(Paragraph((_('Journeys') + ':'), styles['Categories']))
+        for i, journey in enumerate(context['trip'].journey_set.all()):
+            if formatTableCellData(journey.more_info) != '':
+                data.append(Paragraph((str(i + 1) + '. ' + formatTableCellData(journey.more_info)), styles['MyNormal']))
+
+        data.append(Paragraph((_('Accommodations') + ':'), styles['Categories']))
+        for i, accommodation in enumerate(context['trip'].accommodation_set.all()):
+            if formatTableCellData(accommodation.more_info) != '':
+                data.append(Paragraph((str(i + 1) + '. ' + formatTableCellData(accommodation.more_info)), styles['MyNormal']))
+
+        data.append(Paragraph((_('Attractions') + ':'), styles['Categories']))
+        for i, attraction in enumerate(context['trip'].attraction_set.all()):
+            if formatTableCellData(attraction.more_info) != '':
+                data.append(Paragraph((str(i + 1) + '. ' + formatTableCellData(attraction.more_info)),
+                                      styles['MyNormal']))
 
         # create document
         doc.build(data)
