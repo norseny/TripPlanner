b6b3c6aabc127e2249e77ff3f725188e62601a37
bea <beata.piechowicz@student.uj.edu.pl>
2018-06-06 23:32:59 +0200

    Image validation

--- tripplanner/forms/trip_forms.py
+++ tripplanner/forms/trip_forms.py
@@ -5,8 +5,6 @@ from django.utils.translation import gettext as _
 from tripplanner.models import *
 from django import forms
 
-
-
 dict_of_textinput_attrs_autocomplete = {'class': 'form-control form-control-sm autocomplete'}
 dict_of_textinput_attrs = {'class': 'form-control form-control-sm'}
 
@@ -14,16 +12,19 @@ dict_of_textinput_attrs = {'class': 'form-control form-control-sm'}
 class MyDateTimeInput(forms.DateTimeInput):
     input_type = 'date'
 
+
 class MyDateTimeField(forms.DateTimeField):
     widget = MyDateTimeInput(
         attrs={'class': 'form-control form-control-sm datetime'},
         format='%d.%m.%Y (%H:%M)'
     )
 
+
 class MyDateRangeField(forms.CharField):
     widget = TextInput(
         attrs={'class': 'form-control form-control-sm daterange'},
     )
+
 
 class TripForm(ModelForm, Form):
     class Meta:
@@ -34,10 +35,11 @@ class TripForm(ModelForm, Form):
             'description': Textarea(attrs={'rows': 2, 'cols': '65', 'class': 'materialize-textarea form-control '
                                                                              'form-control-sm'}),
             'name': TextInput(attrs=dict_of_textinput_attrs),
-            'private_trip': CheckboxInput(attrs={'class': 'form-check',}),
-            'currency': TextInput({'class': 'form-control form-control-sm', 'style' : 'width:20%'}),
+            'private_trip': CheckboxInput(attrs={'class': 'form-check', }),
+            'currency': TextInput({'class': 'form-control form-control-sm', 'style': 'width:20%'}),
 
         }
+
 
 class JourneyForm(ModelForm):
     start_time = MyDateTimeField(required=False)
@@ -67,8 +69,6 @@ class JourneyForm(ModelForm):
                 self.add_error('start_time', ValidationError(_(str('Start time cannot be later than end time.'))))
 
 
-
-
 class AccommodationForm(ModelForm):
     date_range = MyDateRangeField(required=False)
 
@@ -80,7 +80,8 @@ class AccommodationForm(ModelForm):
             'address': TextInput(attrs=dict_of_textinput_attrs),
             'price': NumberInput(attrs=dict_of_textinput_attrs),
             'more_info': Textarea(
-                attrs={'rows': 2, 'cols': '65', 'class': 'materialize-textarea form-control form-control-sm more-info'}),
+                attrs={'rows': 2, 'cols': '65',
+                       'class': 'materialize-textarea form-control form-control-sm more-info'}),
         }
         exclude = ()
 
@@ -96,11 +97,11 @@ class AttractionForm(ModelForm):
             'address': TextInput(attrs=dict_of_textinput_attrs),
             'price': NumberInput(attrs=dict_of_textinput_attrs),
             'more_info': Textarea(
-                attrs={'rows': 2, 'cols': '65', 'class': 'materialize-textarea form-control form-control-sm more-info'}),
+                attrs={'rows': 2, 'cols': '65',
+                       'class': 'materialize-textarea form-control form-control-sm more-info'}),
 
         }
         exclude = ()
-
 
 
 AttractionFormSet = inlineformset_factory(Trip, Attraction, form=AttractionForm, extra=1)
@@ -109,30 +110,32 @@ AccommodationFormSet = inlineformset_factory(Trip, Accommodation, form=Accommoda
 
 
 class ImageUploadForm(ModelForm):
-    """Image upload form """
+    """Image upload form"""
 
     class Meta:
         model = Trip
         fields = ['main_image']
 
-    # def clean_main_image(self):
-    #     main_image = self.cleaned_data.get('main_image', False)
-    #     if main_image:
-    #         if main_image._size > 2*1024*1024:
-    #             raise ValidationError(_(str("Image file too large")))
-            # return main_image
-        # else:
-        #     raise ValidationError(_(str("Couldn't read uploaded image")))
-
+    def clean_main_image(self):
+        main_image = self.cleaned_data.get('main_image', False)
+        if main_image:
+            if main_image._size > 2 * 1024 * 1024:
+                raise ValidationError(_(str("Image file too large")))
+            return main_image
+        else:
+            return main_image
 
 
 class ProfileForm(ModelForm, Form):
+    """Profile edit form"""
+
     class Meta:
         model = Profile
-        fields = ['email', 'location','about_me' ]
+        fields = ['email', 'location', 'about_me']
         exclude = ()
         widgets = {
-            'about_me': Textarea(attrs={'rows': '10', 'cols': '20', 'class': 'materialize-textarea form-control form-control-sm p-2'}),
+            'about_me': Textarea(
+                attrs={'rows': '10', 'cols': '20', 'class': 'materialize-textarea form-control form-control-sm p-2'}),
             'location': TextInput(attrs=dict_of_textinput_attrs),
             'email': EmailInput(attrs=dict_of_textinput_attrs)
 
